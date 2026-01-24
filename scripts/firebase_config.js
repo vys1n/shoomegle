5b9ca246972d7ee0d9f642829d88563180faafe6
vys1n <kavysingh04@gmail.com>
2026-01-24 15:21:31 +0530

    video v1

---
+++ scripts/firebase_config.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: process.env.FB_API_KEY,
+    authDomain: process.env.FB_AUTH_DOMAIN,
+    projectId: process.env.FB_PROJECT_ID,
+    storageBucket: process.env.FB_STORAGE_BUCKET,
+    messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
+    appId: process.env.FB_APP_ID,
+    measurementId: process.env.FB_MEASUREMENT_ID
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
--- src/services/email.js
+++ src/services/email.js
@@ -15,7 +15,8 @@ const transporter = nodemailer.createTransport({
 });
 
 function isValidEmail(email) {
-    const regex = /^[a-z]+@shooliniuniversity\.com$/;
+    // const regex = /^[a-z]+@shooliniuniversity\.com$/;
+    const regex = /^[a-z0-9]+@[a-z]+\.com$/;
     return regex.test(email);
 }
 
